ca442107fe6076896b414c1bcc3e96b0b24e7b1b
Hui Hon Yu Bryan <bryanhuihy@gmail.com>
2022-11-03 09:28:15 +0800

    added currency converter

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
-# import os
-# import telebot
-# from dotenv import load_dotenv
 from flask import Flask, request, jsonify
-# import requests
+import requests
 
 app = Flask(__name__)
 
@@ -14,12 +11,24 @@ def home():
 def index():
     data = request.get_json()
     monthly_basic_pay = data['queryResult']['parameters']['number']
+    source_currency = data['queryResult']['parameters']['currency-from']
+    amount = data['queryResult']['parameters']['amount']
+    target_currency = data['queryResult']['parameters']['currency-to']
 
+    if monthly_basic_pay:
+        ot_pay = ot_calculation(monthly_basic_pay)
+        response = {
+            'fulfillmentText':"Your hourly OT pay should be ${}\n What else would you like to know?".format(ot_pay)
+        }
 
-    ot_pay = ot_calculation(monthly_basic_pay)
-    response = {
-        'fulfillmentText':"Your hourly OT pay should be {}".format(ot_pay)
-    }
+    elif source_currency:
+        cf = fetch_conversion_factor(source_currency,target_currency)
+        final_amount = amount * cf
+        final_amount = round(final_amount,2)
+        response = {
+            'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+        }
+    
     return jsonify(response)
 
 def ot_calculation(monthly_basic_pay):
@@ -27,34 +36,15 @@ def ot_calculation(monthly_basic_pay):
     hourly_ot_pay = hourly_basic_pay * 1.5
     return('{0:.2f}'.format(hourly_ot_pay))
 
-# def fetch_conversion_factor(source,target):
+def fetch_conversion_factor(source,target):
 
-#     url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9aa0c54f5ad4c460c36d".format(source,target)
+    url = "https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey=9aa0c54f5ad4c460c36d".format(source,target)
 
-#     response = requests.get(url)
-#     response = response.json()
+    response = requests.get(url)
+    response = response.json()
 
-#     return response['{}_{}'.format(source,target)]
+    return response['{}_{}'.format(source,target)]
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# load_dotenv()
-# API_KEY = os.getenv('API_KEY')
-# bot = telebot.TeleBot(API_KEY)
-
-# def OT_calculation(message):
-#     monthly_basic_pay = message.text
-#     hourly_basic_pay = (12 * float(monthly_basic_pay)) / (52 * 44)
-#     hourly_ot_pay = hourly_basic_pay * 1.5
-#     return(hourly_ot_pay)
-
-
-# @bot.message_handler(command=['otcalculator'])
-# def otcalculator(message):
-#     bot.send_message(message, 'Hello, please type in your monthly basic pay:\n')
-#     print(message)
-
-
-# bot.infinity_polling()
